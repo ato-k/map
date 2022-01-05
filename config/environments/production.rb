3a425a84c53083db1e24d12e51329ede60a8197e
Your Name <you@example.com>
2022-01-05 21:11:15 +0900

    deploy13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
-    :domain         => 'heroku.com',
+    :domain         => 'host',
     :enable_starttls_auto => true
   }
 
