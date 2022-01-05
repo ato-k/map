50ac8d0929a824afa53ed2959326c0f7e9df1f30
Your Name <you@example.com>
2022-01-05 20:46:51 +0900

    deploy14

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
+    :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
